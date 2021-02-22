38f34dc7880bcb120c3cb05679d9891233782149
JovanMitrovic <jovan.mitrovic92@hotmail.com>
2021-02-23 00:50:40 +0100

    categories & pagination

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
 use App\Product;
 use Illuminate\Http\Request;
 
@@ -12,11 +13,43 @@ class ShopController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+
+
     public function index()
     {
-        $products = Product::inRandomOrder()->take(12)->get();
+        $pagination = 9;
+        $categories = Category::all();
+        if (request()->category)
+        {
+            $products = Product::with('categories')->whereHas('categories', function ($query){
+               $query->where('slug', request()->category);
+            });
+            $categoryName = optional($categories->where('slug', request()->category)->first())->name;
+        }
+        else
+        {
+            $products = Product::where('featured', true);
+            $categoryName = 'Featured';
+        }
 
-        return view('shop')->with('products', $products);
+        if (request()->sort == 'low_high')
+        {
+            $products = $products->orderBy('price')->paginate($pagination);
+        }
+        elseif (request()->sort == 'high_low')
+        {
+            $products = $products->orderBy('price', 'desc')->paginate($pagination);
+        }
+        else
+        {
+            $products = $products->paginate($pagination);
+        }
+
+        return view('shop')->with([
+            'products' => $products,
+            'categories' => $categories,
+            'categoryName' => $categoryName
+        ]);
     }
 
     /**
